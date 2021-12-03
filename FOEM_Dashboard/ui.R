7a0d1c9adc5328520f04449272adb2da9fdd94c0
ksjeong99 <ksjeong99@users.noreply.github.com>
2021-12-03 16:07:49 -0700

    Update ui.R

--- FOEM_Dashboard/ui.R
+++ FOEM_Dashboard/ui.R
@@ -1,50 +1,8 @@
 library(shiny)
 library(shinythemes)
 
-login_ui <- function(id, title) {
-  
-  ns <- NS(id) # namespaced id
-  
-  # define ui part
-  div(
-    id = ns("login"),
-    style = "width: 500px; max-width: 100%; margin: 0 auto;",
-    
-    div(
-      class = "well",
-      
-      h4(class = "text-center", title),
-      p(class = "text-center", 
-        tags$small("Second approach login form")),
-      
-      textInput(
-        inputId     = ns("ti_user_name_module"),
-        label       = tagList(icon("user"), 
-                              "User Name"),
-        placeholder = "Enter user name"
-      ),
-      
-      passwordInput(
-        inputId     = ns("ti_password_module"), 
-        label       = tagList(
-          icon("unlock-alt"), 
-          "Password"
-        ), 
-        placeholder = "Enter password"
-      ), 
-      
-      div(
-        class = "text-center",
-        actionButton(
-          inputId = ns("ab_login_button_module"), 
-          label   = "Log in",
-          class   = "btn-primary"
-        )
-      )
-    )
-  )
-}
 
+source("module_login.R")
 fluidPage(theme = shinytheme("cyborg"),
           login_ui(id = "module_login", title = "Please login"),
           uiOutput(outputId = "display_content_authr")
