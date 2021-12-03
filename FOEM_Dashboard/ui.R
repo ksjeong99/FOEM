c4d0dd60d15cc4e093922f5a8787f0cb0cb09c97
ksjeong99 <ksjeong99@users.noreply.github.com>
2021-12-03 15:08:49 -0700

    Update ui.R

--- FOEM_Dashboard/ui.R
+++ FOEM_Dashboard/ui.R
@@ -1,68 +1,9 @@
 library(shiny)
 library(shinythemes)
+library(shinyauthr)     # shiny authentication modules
+library(sodium) 
 
 fluidPage(theme = shinytheme("cyborg"),
-          navbarPage(
-            "Freight Operation Efficiency Metric (FOEM)",
-            tabPanel("Calculator",
-                     # Page header
-                     headerPanel('Impact of technology scenarios'),
-                     # side panel
-                     sidebarPanel(
-                       HTML("<h3>Select an analysis level and technology scenarios</h3>"),
-                       selectInput("lv", label = "Unit:", 
-                                   choices = list("Levelized (/miles)" = "level",
-                                                  "Total" = "total"),
-                                   selected = "Levelized (/miles)"), 
-                       # Select group_by
-                       selectInput("group", label = "Analysis Level:", 
-                                   choices = list("Segment-level" = "segment",
-                                                  "System-level" = "system"),
-                                   selected = "Segment-level"),                             
-                       
-                       # Select Frieght Projection
-                       selectInput("s_dmd", label ="Freight Demand Projection:",
-                                   choices = list("BAU-freightProj" ="BAU-freightProj",
-                                                  "Logistical Changes" = "Logistical Changes"),
-                                   selected = "BAU-freightProj"),
-                       
-                       # select Payload factor scenario
-                       selectInput("s_pload", label ="Payload:",
-                                   choices = list("BAU-payload"="BAU-payload",
-                                                  "+10%-payload"="+10%-payload"),
-                                   selected = "BAU-payload"),
-                       
-                       # Select powertrain
-                       selectInput("s_ptrain", label ="Powertrain Adoption:",
-                                   choices = list("AEO21-powertrainAdopt" = "AEO21-powertrainAdopt",
-                                                  "MidZEV-powertrainAdopt" = "MidZEV-powertrainAdopt",
-                                                  "HighZEV-powertrainAdopt" = "HighZEV-powertrainAdopt"), 
-                                   selected = "AEO21-powertrainAdopt"),
-                       # select Connectivity scenario
-                       selectInput("s_conn", label ="Connectivity:",
-                                   choices = list("BAU-connectivity"="BAU-connectivity",
-                                                  "HC-connectivity"="HC-connectivity"),
-                                   selected = "BAU-connectivity"),                               
-                       
-                       # select carbon emissions scenario
-                       selectInput("s_energy", label ="Carbon Emissions:",
-                                   choices = list("Base-wtwEmissions"= "Base-wtwEmissions",               
-                                                  "HighRenewable-wtwEmissions"="HighRenewable-wtwEmissions"),
-                                   selected = "Base-wtwEmissions"),
-                       
-                       actionButton("submitbutton", "Submit", class = "btn btn-primary")
-                     ), # sidebarPanel
-                     mainPanel(
-                       tags$label(h3('Status/Output')), # Status/Output Text Box
-                       htmlOutput("testHTML"),
-                       tabsetPanel(
-                         tabPanel("Energy", plotOutput("plot1")),
-                         tabPanel("Cost", plotOutput("plot2")), 
-                         tabPanel("Emission", plotOutput("plot3")), 
-                         tabPanel("Table", downloadButton("downloadData", label ="Download"), tableOutput("table")))
-                     )), # Navbar 1, tabPanel
-            tabPanel("Methodology", titlePanel("Methodology"),div(includeMarkdown("TechD.md"), align="justify")),
-            tabPanel("About", titlePanel("About"), div(includeMarkdown("About.md"), align="justify"))
-            
-          ) # navbarPage
-) # fluidPage
+          shinyauthr::loginUI(id = "login"),
+          uiOutput(outputId = "display_content_authr")
+         ) # fluidPage
